021137fc1cc98698faabcaaa0ca7d164cf87a3a2
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2021-01-27 20:54:18 +0300

    day 2

--- src/js/components/header.js
+++ src/js/components/header.js
@@ -1,9 +1,23 @@
-var fixNav = 50;
-	$(window).scroll(function () {
-		var scroll = $(this).scrollTop();
-		if (scroll >= fixNav) {
-			$('.header').addClass('header--sticky');
-		} else {
-			$('.header').removeClass('header--sticky');
-		}
-	});
+// var fixNav = 50;
+// 	$(window).scroll(function () {
+// 		var scroll = $(this).scrollTop();
+// 		if (scroll >= fixNav) {
+// 			$('.header').addClass('header--sticky');
+// 		} else {
+// 			$('.header').removeClass('header--sticky');
+// 		}
+// 	});
+
+$('.header__menu a').on('click', function (e) {
+	// e.preventDefault();
+
+	$('.header__menu a').removeClass('active').filter(this).addClass('active');
+
+	// var selector = $(this).attr('href');
+	// var h = $(selector);
+
+	// $('html, body').animate({
+	// 	scrollTop: h.offset().top - 40
+	// }, 400);
+
+});
