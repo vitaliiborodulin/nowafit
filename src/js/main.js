7916c525fac63af18d88bd4e1296d33849fb676f
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2021-01-30 11:06:04 +0300

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,3 @@
-//= ../../node_modules/jquery/dist/jquery.min.js
 //= ../../node_modules/owl.carousel/dist/owl.carousel.min.js
 //= ../../node_modules/magnific-popup/dist/jquery.magnific-popup.min.js
 
